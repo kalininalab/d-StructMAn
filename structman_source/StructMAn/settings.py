3db538143f6928c8fdf13318885d75311389ee75
Alexander Gress <alexander.gress@helmholtz-hips.de>
2021-09-06 15:40:09 +0200

    Added other main folder files

---
+++ structman_source/StructMAn/settings.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import structman
+import structman.lib as lib
+from structman.lib import rinerator
+from structman import scripts
+
+
+ROOT_DIR = Path(structman.__file__).parent.absolute()
+LIB_DIR = Path(lib.__file__).parent.absolute()
+RINERATOR_DIR = Path(rinerator.__file__).parent.absolute()
+SCRIPTS_DIR = Path(scripts.__file__).parent.absolute()
+
+RINERATOR_PATH = f'{RINERATOR_DIR}/get_chains.py'
+
+UNIPROT_DB_SQL = f'{SCRIPTS_DIR}/struct_man_db_uniprot.sql.gz'
+STRUCTMAN_DB_SQL = f'{SCRIPTS_DIR}/database_structure.sql'
+REDUCE_HET_DICT = f'{RINERATOR_DIR}/reduce_wwPDB_het_dict.txt'
+PDB_SYNC_SCRIPT = f'{SCRIPTS_DIR}/pdb-rsync.sh'
